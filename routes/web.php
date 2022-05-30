e0ac45f8b0c1bdfa979a20c4be343eab85f7d971
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-05-30 17:28:01 +0300

    add urls routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/urls', [UrlController::class, 'index']);
+Route::post('/urls', [UrlController::class, 'add']);
+Route::get('/urls/{id}', [UrlController::class, 'show'])->name('showUrl');
