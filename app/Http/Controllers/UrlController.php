d29857fc4530ee305ceef2574a1a0023c3643dd1
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-05-31 15:44:25 +0300

    add validation rules

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
 class UrlController extends Controller
@@ -17,40 +15,94 @@ class UrlController extends Controller
      */
     public function index()
     {
-        
+        //
     }
 
     /**
-     * Either add new url to database and redirect to it's page or show existing url page .
-     * 
-     * @param  \Illuminate\Http\Request $request
+     * Show the form for creating a new resource.
+     *
      * @return \Illuminate\Http\Response
      */
-    public function add(Request $request)
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
         $data = $request->input('url');
         $name = $data['name'];
+
+        $validated = $request->validate([
+            'url[name]' => 'required|max:255'
+        ]);
 
         $record = DB::table('urls')
             ->select('id')
             ->where('name', $name)
             ->get();
-        $id = $record[0]->id;
 
-        if (empty($id)) {
+        if (empty($record[0])) {
             $dateTime = Carbon::now()->toDateTimeString();
             $id = DB::table('urls')->insertGetId([
                 'name' => $name,
                 'created_at' => $dateTime
             ]);
+        } else {
+            $id = $record[0]->id;
         }
 
         return redirect()->route('showUrl', ['id' => $id]);
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
-
         return view('show', ['id' => $id]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
