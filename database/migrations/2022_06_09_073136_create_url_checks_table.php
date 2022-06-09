b0e563f9170754238a18e49e4b6b3502287a7343
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-06-09 10:46:40 +0300

    create url_checks creating migration

---
+++ database/migrations/2022_06_09_073136_create_url_checks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('url_checks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('status_code');
+            $table->string('h1');
+            $table->string('title');
+            $table->string('description');
+            $table->timestamp('created_at');
+            $table->foreignId('url_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('url_checks');
+    }
+};
