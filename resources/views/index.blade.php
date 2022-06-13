aa9ceff4ec0824d39ecd349b248d76b4100e4a2c
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-06-13 10:09:07 +0300

    fix welcome page and add css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Page analyzer</title>
-
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-    <body>
-        @include('flash::message')
-        <main>
-            {{-- <div class="container"> --}}
-                <h1>Page analyzer</h1>
-                <form action="{{ route('urls.store')}} " method="POST">
-                    @csrf
-                    <input type="text" placeholder="https://example.com" name="url[name]">
-                    <input type="submit" class="btn btn-primary" value="Check">
-                </form>
-            {{-- </div> --}}
-        </main>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+@include('flash::message')
+    <main class="flex-grow-1">
+        <div class="container-lg mt-3">
+            <div class="row">
+                <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded-3 bg-light p-5">
+                    <h1 class="display-3">Анализатор страниц</h1>
+                    <p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>
+                    <form action="{{ route('urls.store')}} " method="POST" class="d-flex justify-content-center">
+                        @csrf
+                        <input type="text" name="url[name]" class="form-control form-control-lg" placeholder="https://www.example.com">
+                        <input type="submit" class="btn btn-primary btn-lg ms-3 px-5 text-uppercase mx-3" value="Проверить">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
