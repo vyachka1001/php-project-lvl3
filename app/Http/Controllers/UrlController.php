4ab556234671ee2c77b368ed2aa30dfd25ff0f57
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-06-15 14:21:00 +0300

    rm unnecessary methods

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -36,16 +36,6 @@ class UrlController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('index');
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request contains url info from form
@@ -106,42 +96,5 @@ class UrlController extends Controller
             ->get();
 
         return view('url.show', ['url' => $url[0], 'checks' => $checks]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * 
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * 
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * 
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
