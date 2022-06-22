28b1db5a8342f606d29e30c8a88bcff787e48d6f
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-06-22 16:48:10 +0300

    add checks for null selecting and add methods parameters and returning values types

--- app/Repositories/UrlRepository.php
+++ app/Repositories/UrlRepository.php
@@ -7,59 +7,94 @@ use Carbon\Carbon;
 
 class UrlRepository
 {
-    public function findPaginatedUrls(int $urlPerPage)
+    /**
+     * Returns oldest urls.
+     * @param int $urlPerPage Count of returning urls. If count is more than records, returns all records.
+     * 
+     * @return \Illuminate\Pagination\LengthAwarePaginator
+     */
+    public function findPaginatedUrls(int $urlPerPage): \Illuminate\Pagination\LengthAwarePaginator
     {
         return DB::table('urls')
             ->oldest()
             ->paginate($urlPerPage);
     }
 
-    public function findIdByName(string $name)
+    /**
+     * Returns id by corresponding name.
+     * If there is no such name in db, returns null.
+     * 
+     * @param string $name corresponding url's name.
+     * 
+     * @return int|null
+     */
+    public function findIdByName(string $name): ?int
     {
         $record = DB::table('urls')
             ->select('id')
             ->where('name', $name)
             ->get();
 
-        return $record[0]->id;
+        return $record[0]->id ?? null;
     }
 
+    /**
+     * Inserts url into db.
+     * Returns true, if data insertion is completed.
+     * 
+     * @param int $name Corresponding url.
+     * 
+     * @return bool
+     */
     public function save(string $name): bool
     {
-        $record = DB::table('urls')
-            ->select('id')
-            ->where('name', $name)
-            ->get();
+        $id = $this->findIdByName($name);
 
-        if (!empty($record[0])) {
+        if (!empty($id)) {
             return false;
         }
 
         $dateTime = Carbon::now()->toDateTimeString();
-        $id = DB::table('urls')->insertGetId(
+        $result = DB::table('urls')->insert(
             [
                 'name' => $name,
                 'created_at' => $dateTime
             ]
         );
 
-        return true;
+        return $result;
     }
 
-    public function findById(int $id)
+     /**
+     * Returns record by corresponding id
+     * Returns empty collection, if there is no such id.
+     * 
+     * @param int $id Corresponding id.
+     * 
+     * @return \Illuminate\Support\Collection
+     */
+    public function findById(int $id): \Illuminate\Support\Collection
     {
         return DB::table('urls')->select('*')
             ->where('id', $id)
             ->get();
     }
 
-    public function findNameById(int $id)
+    /**
+     * Returns name by corresponding id.
+     * If there is no such id in db, returns null.
+     * 
+     * @param int $id corresponding url's id.
+     * 
+     * @return string|null
+     */
+    public function findNameById(int $id): ?string
     {
         $url = DB::table('urls')
             ->select('name')
             ->where('id', $id)
             ->get();
 
-        return $url[0]->name;
+        return $url[0]->name ?? null;
     }
 }
